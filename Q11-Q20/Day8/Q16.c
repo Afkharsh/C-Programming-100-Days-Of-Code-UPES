501a0fa423cfcb05a90c86f23e510b8c5c8118e2
Harsh <154446476+Afkharsh@users.noreply.github.com>
2025-11-09 16:33:06 +0530

    Q16.c

---
+++ Q11-Q20/Day8/Q16.c
@@ -0,0 +1,24 @@
+// Write a program to input three numbers and find the largest among them using if–else.
+//Name: HARSH
+//SAP ID: 590028742
+
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a >= b && a >= c)
+    {
+        printf("Largest is %d\n", a);
+    }
+    else if (b >= a && b >= c)
+    {
+        printf("Largest is %d\n", b);
+    }
+    else
+    {
+        printf("Largest is %d\n", c);
+    }
+    return 0;
+}
